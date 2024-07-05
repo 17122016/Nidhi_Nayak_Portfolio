a1279e53a5aabf42804f9000ac839167d8d78200
Nidhi Nayak <60571612+17122016@users.noreply.github.com>
2024-07-05 12:04:32 +0000

    update about and Footer section

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -10,14 +10,16 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import devDotToIcon from "../images/socials/devdotto.svg";
+
 import envelopeIcon from "../images/socials/envelope.svg";
 import gitHubIcon from "../images/socials/github.svg";
-import instagramIcon from "../images/socials/instagram.svg";
 import linkedInIcon from "../images/socials/linkedin.svg";
-import mediumIcon from "../images/socials/medium.svg";
-import twitterIcon from "../images/socials/twitter.svg";
-import youTubeIcon from "../images/socials/youtube.svg";
+
+// import devDotToIcon from "../images/socials/devdotto.svg";
+// import instagramIcon from "../images/socials/instagram.svg";
+// import mediumIcon from "../images/socials/medium.svg";
+// import twitterIcon from "../images/socials/twitter.svg";
+// import youTubeIcon from "../images/socials/youtube.svg";
 
 /**
  * 💡 Learning resources
@@ -28,16 +30,16 @@ import youTubeIcon from "../images/socials/youtube.svg";
 
 const Footer = (props) => {
   const {
-    devDotTo,
     email = "nidhinayak154@gmail.com",
-    gitHub = "https://github.com/17122016",
-    instagram,
-    linkedIn = "https://www.linkedin.com/in/nidhi-nayak-688a9718b/",
-    medium,
-    name,
-    primaryColor,
-    twitter,
-    youTube,
+    gitHub = "17122016",
+    linkedIn = "nidhi-nayak-688a9718b",
+    name = "Nidhi Nayak",
+    // devDotTo,
+    // medium,
+    // instagram,
+    // primaryColor,
+    // twitter,
+    // youTube,
   } = props;
 
   return (
@@ -65,17 +67,17 @@ const Footer = (props) => {
             <img src={envelopeIcon} alt="email" className="socialIcon" />
           </a>
         )}
-        {devDotTo && (
+        {/* {devDotTo && (
           <a href={`https://dev.to/${devDotTo}`} target="_blank" rel="noopener noreferrer">
             <img src={devDotToIcon} alt="Dev.to" className="socialIcon" />
           </a>
-        )}
+        )} */}
         {gitHub && (
           <a href={`https://github.com/${gitHub}`} target="_blank" rel="noopener noreferrer">
             <img src={gitHubIcon} alt="GitHub" className="socialIcon" />
           </a>
         )}
-        {instagram && (
+        {/* {instagram && (
           <a
             href={`https://www.instagram.com/${instagram}`}
             target="_blank"
@@ -83,7 +85,7 @@ const Footer = (props) => {
           >
             <img src={instagramIcon} alt="Instagram" className="socialIcon" />
           </a>
-        )}
+        )} */}
         {linkedIn && (
           <a
             href={`https://www.linkedin.com/in/${linkedIn}`}
@@ -93,12 +95,12 @@ const Footer = (props) => {
             <img src={linkedInIcon} alt="LinkedIn" className="socialIcon" />
           </a>
         )}
-        {medium && (
+        {/* {medium && (
           <a href={`https://medium.com/@${medium}`} target="_blank" rel="noopener noreferrer">
             <img src={mediumIcon} alt="Medium" className="socialIcon" />
           </a>
-        )}
-        {twitter && (
+        )} */}
+        {/* {twitter && (
           <a href={`https://twitter.com/${twitter}`} target="_blank" rel="noopener noreferrer">
             <img src={twitterIcon} alt="Twitter" className="socialIcon" />
           </a>
@@ -111,7 +113,7 @@ const Footer = (props) => {
           >
             <img src={youTubeIcon} alt="YouTube" className="socialIcon" />
           </a>
-        )}
+        )} */}
       </div>
       <p className="small" style={{ marginTop: 0, color: "white" }}>
         Created by {name}
